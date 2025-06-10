201b9cb3e3415d25e2e15d715d3db3ac48c89df0
Miguel Espinosa <mikespinosa@MacBook-Pro-de-Miguel.local>
2025-06-10 08:12:05 -0600

    refactor: Adjust layout and text on contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -40,21 +40,21 @@ export default function Contact() {
         <div className="p-12 xs:w-1/2 xs:p-0">
           <div className="flex justify-between uppercase">
             <p className="m-0">Miguel</p>
-            <p className="m-0">Angel</p>
+            <p className="m-0">Ángel</p>
           </div>
           <div className="flex justify-between uppercase">
-            <p className="m-0">Ing. en</p>
-            <p className="m-0">Tecnologías de</p>
+            <p className="m-0">Ingeniero en T.I.</p>
+            <p className="m-0">y</p>
           </div>
           <div className="flex justify-between uppercase">
-            <p className="m-0">la Información</p>
-            <p className="m-0">y Negocios</p>
-          </div>
-          <div className="flex justify-between uppercase">
+            <p className="m-0">Negocios</p>
             <p className="m-0">Digitales</p>
+          </div>
+          <div className="flex justify-between uppercase">
+            <p className="m-0">| MEX</p>
             <Link href={'https://www.linkedin.com/in/miguel-angel-espinosa-de-los-monteros-283bb930b/'}>
               <TextDisperse setBackground={setBackground}>
-                <p>→LinkedIn | MEX</p>
+                <p>→LinkedIn</p>
               </TextDisperse>
             </Link>
           </div>
